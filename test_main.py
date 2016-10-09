ad600594e1fbbe10adc2e00a72395b26daf9d8ce
Roopeswar Kommalapati <roopeshkom@gmail.com>
2016-10-08 22:45:26 -0400

    Delete test_main.py

--- test_main.py
+++
@@ -1,9 +0,0 @@
-from ai_client import *
-import json
-from StringIO import StringIO
-
-obj = AICLIENT_get_json_obj_remote("https://staticdelivery.nexusmods.com/mods/110/images/74627-0-1459502036.jpg")
-
-tag = AICLIENT_get_nth_json_result(obj, 0)
-
-print AICLIENT_get_wikipedia_desc(tag)
