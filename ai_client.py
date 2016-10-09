aa944980e0e22e2d91366531a27b5a59135558ec
Roopeswar Kommalapati <roopeshkom@gmail.com>
2016-10-08 23:16:28 -0400

    Update ai_client.py

--- ai_client.py
+++ ai_client.py
@@ -2,6 +2,13 @@ from clarifai.client import ClarifaiApi
 import wikipedia
 import json
 
+def get_name_and_description(image):
+    guess = AICLIENT_get_nth_json_result(AICLIENT_get_json_obj_local(image), 0)
+    retString = "Your picture is most likely a %s.\n" % guess
+    retString += AICLIENT_get_wikipedia_desc(guess)
+    return retString
+
+
 def AICLIENT_get_json_obj_local(filename):
     api = ClarifaiApi();
     result = api.tag_images(open(filename), "rb")
@@ -19,4 +26,3 @@ def AICLIENT_get_nth_json_result(json_obj, n):
 def AICLIENT_get_wikipedia_desc(tag):
     page = wikipedia.summary(tag, 1)
     return page
-
